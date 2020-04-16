3275770b3ff6b129cc1a1c5f28395b1889e83dc7
jonilsonds9 <jonilsonds9@gmail.com>
2020-04-16 17:53:11 -0300

    fim da quarta aula

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -6,28 +6,28 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event) {
     event.preventDefault();
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormalario();
+  }
 
-    let data = new Date(...
-      this._inputData.value
-        .split('-')
-        .map((item, indice) => item - indice % 2)
-    );
-
-    let negociacao = new Negociacao(
-      data,
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
-      this._inputValor.value
-    );
+      this._inputValor.value);
+  }
 
-    console.log(negociacao);
+  _limpaFormalario() {
+    this._inputData.value = '';
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0;
 
-    // adicionar lista
-
-
+    this._inputData.focus();
   }
 
 }
